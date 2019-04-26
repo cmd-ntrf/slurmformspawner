0fc5f73873a70f185a02f3f60d797faf79b40cba
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2019-04-26 18:35:58 -0400

    Bump to version 0.8.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 setup_args = dict(
     name                = 'slurmformspawner',
     packages            = ['slurmformspawner'],
-    version             = "0.7.3",
+    version             = "0.8.0",
     description         = "slurmformspawner: JupyterHub SlurmSpawner with a dynamic spawn form",
     author              = "Félix-Antoine Fortin",
     author_email        = "felix-antoine.fortin@calculquebec.ca",
