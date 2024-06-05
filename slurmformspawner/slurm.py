57fb46cc2fa211bded9ef724c1eb7ab05eacd9d6
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2024-06-05 15:09:07 -0400

    Use --json flag to retrieve features with scontrol
    Black slurm.py

--- slurmformspawner/slurm.py
+++ slurmformspawner/slurm.py
@@ -1,3 +1,4 @@
+from json import loads
 from operator import attrgetter
 from traitlets.config import SingletonConfigurable
 from traitlets import Integer
@@ -6,6 +7,7 @@ from datetime import datetime
 from subprocess import check_output, CalledProcessError
 
 from cachetools import TTLCache, cachedmethod
+
 
 class SlurmAPI(SingletonConfigurable):
     info_cache_ttl = Integer(300).tag(config=True)
@@ -16,78 +18,98 @@ class SlurmAPI(SingletonConfigurable):
     def __init__(self, config=None):
         super().__init__(config=config)
         self.info_cache = TTLCache(maxsize=1, ttl=self.info_cache_ttl)
-        self.acct_cache = TTLCache(maxsize=self.acct_cache_size, ttl=self.acct_cache_ttl)
+        self.acct_cache = TTLCache(
+            maxsize=self.acct_cache_size, ttl=self.acct_cache_ttl
+        )
         self.res_cache = TTLCache(maxsize=1, ttl=self.res_cache_ttl)
 
-    @cachedmethod(attrgetter('info_cache'))
+    @cachedmethod(attrgetter("info_cache"))
     def get_node_info(self):
-        output = {'cpu': [], 'mem': [], 'gres': [], 'partitions': []}
+        output = {
+            "cpu": set(),
+            "mem": set(),
+            "gres": set(),
+            "partitions": set(),
+            "features": set(),
+        }
         try:
-            controls = check_output(['scontrol', '-o', 'show', 'node'], encoding='utf-8')
+            show_node_str = check_output(
+                ["scontrol", "-o", "show", "node", "--json"], encoding="utf-8"
+            )
         except CalledProcessError:
             return output
-        else:
-            nodes = [
-                dict([
-                    item.split('=', 1) for item in line.split(' ') if '=' in item
-                ])
-                for line in controls.split("\n") if line
-            ]
-            for node in nodes:
-                output['cpu'].append(int(node['CPUTot']))
-                output['mem'].append(int(node['RealMemory']) - int(node.get('MemSpecLimit', '0')))
-                output['gres'].extend(node['Gres'].split(","))
-                output['partitions'].extend(node['Partitions'].split(","))
+
+        show_node_map = loads(show_node_str)
+        for node in show_node_map["nodes"]:
+            output["cpu"].add(node["cpus"])
+            output["mem"].add(node["real_memory"] - node.get("specialized_memory", 0))
+            output["gres"].update(node["gres"].split(","))
+            output["partitions"].update(node["partitions"])
+            output["features"].update(node["features"])
         return output
 
     def is_online(self):
-        return self.get_node_info()['cpu'] and self.get_node_info()['mem']
+        return self.get_node_info()["cpu"] and self.get_node_info()["mem"]
 
     def get_cpus(self):
-        cpus = set(self.get_node_info()['cpu'])
-        return sorted(cpus)
+        return sorted(self.get_node_info()["cpu"])
 
     def get_mems(self):
-        mems = set(self.get_node_info()['mem'])
-        return sorted(mems)
+        return sorted(self.get_node_info()["mem"])
 
     def get_gres(self):
-        gres = set(self.get_node_info()['gres'])
-        if '(null)' in gres:
-            gres.remove('(null)')
-        return ['gpu:0'] + sorted(gres)
+        gres = self.get_node_info()["gres"].copy()
+        if "(null)" in gres:
+            gres.remove("(null)")
+        return ["gpu:0"] + sorted(gres)
 
     def get_partitions(self):
-        partitions = set(self.get_node_info()['partitions'])
-        return sorted(partitions)
+        return sorted(self.get_node_info()["partitions"])
 
-    @cachedmethod(attrgetter('acct_cache'))
+    @cachedmethod(attrgetter("acct_cache"))
     def get_accounts(self, username):
         try:
-            string = check_output(['sacctmgr', 'show', 'user', username, 'withassoc',
-                                    'format=account', '-P', '--noheader'], encoding='utf-8')
+            string = check_output(
+                [
+                    "sacctmgr",
+                    "show",
+                    "user",
+                    username,
+                    "withassoc",
+                    "format=account",
+                    "-P",
+                    "--noheader",
+                ],
+                encoding="utf-8",
+            )
         except CalledProcessError:
             return []
         return string.split()
 
-    @cachedmethod(attrgetter('res_cache'))
+    @cachedmethod(attrgetter("res_cache"))
     def get_reservations(self):
         try:
-            reservations = check_output(['scontrol', 'show', 'res', '-o', '--quiet'], encoding='utf-8')
+            reservations = check_output(
+                ["scontrol", "show", "res", "-o", "--quiet"], encoding="utf-8"
+            )
         except CalledProcessError:
             reservations = []
         else:
             if reservations:
-                reservations = reservations.strip().split('\n')
+                reservations = reservations.strip().split("\n")
             else:
                 reservations = []
 
-        reservations = [dict([item.split('=', maxsplit=1) for item in res.split()]) for res in reservations if res]
+        reservations = [
+            dict([item.split("=", maxsplit=1) for item in res.split()])
+            for res in reservations
+            if res
+        ]
         for res in reservations:
-            res['Users'] = set(res['Users'].split(','))
-            res['Accounts'] = set(res['Accounts'].split(','))
-            res['StartTime'] = datetime.strptime(res['StartTime'], "%Y-%m-%dT%H:%M:%S")
-            res['EndTime'] = datetime.strptime(res['EndTime'], "%Y-%m-%dT%H:%M:%S")
+            res["Users"] = set(res["Users"].split(","))
+            res["Accounts"] = set(res["Accounts"].split(","))
+            res["StartTime"] = datetime.strptime(res["StartTime"], "%Y-%m-%dT%H:%M:%S")
+            res["EndTime"] = datetime.strptime(res["EndTime"], "%Y-%m-%dT%H:%M:%S")
         return reservations
 
     def get_active_reservations(self, username, accounts):
@@ -98,12 +120,13 @@ class SlurmAPI(SingletonConfigurable):
         accounts = set(accounts)
         now = datetime.now()
         return [
-            res for res in reservations
+            res
+            for res in reservations
             if (
-                res['StartTime'] <= now <= res['EndTime'] and
-                (
-                    username in res['Users'] or
-                    bool(accounts.intersection(res['Accounts']))
+                res["StartTime"] <= now <= res["EndTime"]
+                and (
+                    username in res["Users"]
+                    or bool(accounts.intersection(res["Accounts"]))
                 )
             )
         ]
